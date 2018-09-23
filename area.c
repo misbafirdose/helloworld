3a41e7949e4fc946846c28bb27e5f5f081a19fa3
misbafirdose <43476991+misbafirdose@users.noreply.github.com>
2018-09-23 23:08:05 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a,b,h;
+clrscr();
+printf("entet the value of b,h\n");
+scanf("%f%f",&b,&h);
+a=(0.5)*(b*h);
+printf("the vale of a=%f%f\n);
+getch();
+}
